6093afd2ddc92e4cbd5efcba681b562d3496b0fc
Kapil-dhakad <dhakadkapil350@gmail.com>
2025-08-24 22:11:47 +0530

    route change

--- backend4/src/app.js
+++ backend4/src/app.js
@@ -45,7 +45,7 @@ app.get('/posts', async (req, res) => {
     })
 })
 
-app.get('*', (req, res)=> {
+app.get('*name', (req, res)=> {
     res.sendFile(path.join(__dirname, '..', 'public', 'index.html'))
 })
 
